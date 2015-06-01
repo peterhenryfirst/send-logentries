4421775ead2c2f512327902458ecfffff5952b77
Pedro Pereira <pedropereira@MacBook-Pro-de-Pedro.local>
2015-06-01 18:26:12 +0200

    First steps to send logs

---
+++ test/index.js
@@ -0,0 +1,8 @@
+var should = require('chai').should(),
+	send-logentries = require('../index');
+
+/*
+describe('init module', function () {
+	it('initialize with default token', function () {});
+});
+*/
